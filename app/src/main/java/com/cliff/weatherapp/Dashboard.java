967de4fe13775631c91939bc6490c0f4e1791029
colleowino <colleowino@gmail.com>
2017-08-18 13:54:51 +0300

    added a loading screen

--- app/src/main/java/com/cliff/weatherapp/Dashboard.java
+++ app/src/main/java/com/cliff/weatherapp/Dashboard.java
@@ -8,6 +8,7 @@ public class Dashboard extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_main);
     }
+
 }
