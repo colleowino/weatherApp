03ad0c3c5520ec4b604c79db1280af44075050bd
colleowino <colleowino@gmail.com>
2017-08-18 15:03:45 +0300

    loads most data

--- app/src/main/java/com/cliff/weatherapp/Dashboard.java
+++ app/src/main/java/com/cliff/weatherapp/Dashboard.java
@@ -1,15 +1,31 @@
 package com.cliff.weatherapp;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.location.*;
-import android.util.Log;
-import java.util.List;
-
 import android.app.AlertDialog;
-import android.provider.Settings;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -19,15 +35,27 @@ public class Dashboard extends AppCompatActivity {
     final String TAG = "GPS";
 
     LocationManager locationManager;
+    TextView tvLatitude, tvLongitude, tvWeather, tvDate, tvTemp_c, tvTemp_f;
+    RelativeLayout vProgressLayer;
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tvLatitude = (TextView) findViewById(R.id.tvLatitude);
+        tvLongitude = (TextView) findViewById(R.id.tvLongitude);
+        tvWeather = (TextView) findViewById(R.id.tvWeather);
+        tvDate = (TextView) findViewById(R.id.tvDate);
+        tvTemp_c = (TextView) findViewById(R.id.tvTemp_c);
+        tvTemp_f = (TextView) findViewById(R.id.tvTemp_f);
+        vProgressLayer = (RelativeLayout) findViewById(R.id.progressLayer);
+
         locationManager = (LocationManager)getApplicationContext().getSystemService(LOCATION_SERVICE);
         isGPS = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
         isNetwork = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        requestQueue = Volley.newRequestQueue(this);
 
         if (!isGPS && !isNetwork) {
             Log.d(TAG, "Connection off");
@@ -41,6 +69,7 @@ public class Dashboard extends AppCompatActivity {
             lat = Double.toString(myLocation.getLatitude());
             lon = Double.toString(myLocation.getLongitude());
             Log.d(TAG,"DAta: "+myLocation);
+            fetchLocationData();
         }
     }
 
@@ -62,6 +91,47 @@ public class Dashboard extends AppCompatActivity {
         return bestLocation;
     }
 
+    public void fetchLocationData(){
+        String url = "https://fcc-weather-api.glitch.me/api/current?lat="+lat+"&lon="+lon;
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG,"DAta: "+response);
+
+                        try {
+                            String locationTemp = response.getJSONObject("main").getString("temp");
+                            String locationWeather = response.getJSONArray("weather").getJSONObject(0).getString("main");
+
+                            String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+
+                            tvWeather.setText(locationWeather);
+                            tvTemp_c.setText(locationTemp + " \u2103");
+                            Double fahrenheit = Double.valueOf(locationTemp)*1.8 + 32;
+                            tvTemp_f.setText(fahrenheit + " \u2109");
+                            tvLatitude.setText(lat);
+                            tvLongitude.setText(lon);
+                            tvDate.setText(currentDateTimeString);
+                            vProgressLayer.setVisibility(View.GONE);
+
+                        } catch (JSONException e) {
+                            //some exception handler code.
+                            Log.d(TAG, "error occured: "+e);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e(TAG, "Something went wrong: "+error);
+                    }
+                });
+
+        //add request to queue
+        requestQueue.add(jsonObjectRequest);
+
+    }
 
     public void showSettingsAlert() {
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
