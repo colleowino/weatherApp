62196c6279a69010811439cac564dcc4ff2164bf
colleowino <colleowino@gmail.com>
2017-08-18 13:17:20 +0300

    fixed the styling

--- app/src/main/java/com/cliff/weatherapp/LocationInfo.java
+++ app/src/main/java/com/cliff/weatherapp/LocationInfo.java
@@ -18,6 +18,7 @@ public class LocationInfo extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
     }
 
     @Override
